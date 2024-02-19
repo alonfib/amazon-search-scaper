273cb5bf573cc457f690d29ff40bcd6d5da20aed
Alon Fibert <alonfib@gmail.com>
2024-02-19 03:07:28 +0200

    removed unused

--- app/components/Scraper/Scraper.client.tsx
+++ app/components/Scraper/Scraper.client.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Button from "../Button/Button.client";
 import useSWR from "swr";
 import SuggestionsList, { Suggestions } from "./innerComponents/SuggestionsList/SuggestionsList";
--- pages/api/amazon/index.ts
+++ pages/api/amazon/index.ts
@@ -1,4 +1,4 @@
-import puppeteer, { Browser, Page } from "puppeteer";
+import puppeteer, { Page } from "puppeteer";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const allLetters = [
